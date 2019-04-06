31f923d142a96bb056b7d50bd2b1c24be6786fb2
2020vbo <victorbo2020@gmail.com>
2019-04-06 17:19:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,10 @@ app.use('/img', express.static(path.join(__dirname, 'img')));
 app.get('/', function(req, res){
     res.render('index');
 });
+
+// -------------- listener -------------- //
+// // The listener is what keeps node 'alive.' 
+
+var listener = app.listen(app.get('port'), function() {
+  console.log( 'Express server started on port: '+listener.address().port );
+});
